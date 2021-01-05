d20522e69e17a66f44259cf1eaae7e765d73e016
MabiGermano <mabi.germano.e@gmail.com>
2021-01-05 02:08:15 -0300

    refatoração do código javascript inicial

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
-function teste () {
-    const body = document.querySelector('body');
-    console.log(body);
-    body.style.backgroundImage = `url('https://picsum.photos/1200/720')`;
+document.addEventListener('DOMContentLoaded', function () {
+    const body = document.body;
+    body.style.backgroundImage = `url(
+        'https://picsum.photos/${body.clientWidth}/${body.clientHeight}'
+        )`;
     body.style.backgroundSize = 'cover';
     body.style.backgroundAttachment = 'fixed';
-    fetch(`https://picsum.photos/v2/list`).then((response) => {
-        
-        return response.json();
-        
-    }).then((responseJson) => {
-        
-        //injectInfo(responseJson);
-        console.log(responseJson);
-        
-    });
-}
-teste();    
+});
